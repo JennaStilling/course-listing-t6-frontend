bd0de9c43dc6cd29d46c2a3d207e536be189e2fe
HeyBail <Bill331333@gmail.com>
2024-09-19 15:21:03 -0500

    added basic vue pages and router (You will still need to npm install the dependencies on your local machine, the git ignore ignores them)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import router from "./router.js";
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import FirstPage from './views/CourseList.vue' //directory for first page
+
+createApp(FirstPage).mount('#app')
---
+++ src/views/CourseList.vue
@@ -0,0 +1,13 @@
+<script setup>
+import CourseServices from "../services/CourseServices.js";
+import { ref } from "vue";
+</script>
+
+<template>
+    <div>
+        hello, Course List
+    </div>
+    <div>
+        
+    </div>
+</template>
