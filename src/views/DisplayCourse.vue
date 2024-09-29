11d6cd4fbefd785fc278761cf762ec04ef239c67
ari.t <arriannaomi.a.taton@eagles.oc.edu>
2024-09-29 18:03:20 -0500

    Changed the response.data to get the first in the response instead of the course

--- src/views/DisplayCourse.vue
+++ src/views/DisplayCourse.vue
@@ -16,9 +16,10 @@ const course = ref({});
 const errors = ref({});
 
 onMounted(() => {
+  //either props.id is not working or getCourse is not
   CourseServices.getCourse(props.id)
-    .then(response => {
-      course.value = response.data.course;
+    .then((response) => {
+      course.value = response.data[0];
       message.value = "";
     })
     .catch((error) => {
@@ -43,64 +44,38 @@ function cancel() {
       <br />
       <div class="form">
         <div class="form-group">
-          <label for="department">
-            Department
-            <span id="dmptErr" class="text-error">{{
-             errors.department || "*"
-            }}</span>
-          </label>
+          <label for="department">Department</label>
           <input v-model="course.department" type="text" id="department" />
         </div>
   
         <div class="form-group">
-          <label for="courseNumber">
-            Course Number
-            <span id="courseNumError" class="text-error">{{
-             errors.courseNumber || "*"
-            }}</span>
-          </label>
+          <label for="courseNumber">Course Number</label>
           <input v-model="course.courseNumber" type="text" id="courseNumber" />
         </div>
   
         <div class="form-group">
-          <label for="level">
-            Level
-            <span id="levelErr" class="text-error">{{
-             errors.level || "*"
-            }}</span>
-          </label>
+          <label for="level">Level</label>
           <input v-model="course.level" type="text" id="level" />
         </div>
   
         <div class="form-group">
-          <label for="hours">
-            Hours
-            <span id="hoursErr" class="text-error">{{
-             errors.hours || "*" 
-            }}</span>
-          </label>
+          <label for="hours">Hours</label>
           <input v-model="course.hours" type="text" id="hours"/>
         </div>
   
         <div class="form-group">
-          <label for="name">
-            Name
-            <span id="nameErr" class="text-error">{{
-             errors.name || "*" 
-            }}</span>
-          </label>
+          <label for="name">Name</label>
           <input v-model="course.name" type="text" id="name" />
         </div>
   
         <div class="form-group">
-          <label for="description">
-            Description
-            <span id="descriptionErr" class="text-error">{{ errors.state || "*"}}</span>
-          </label>
+          <label for="description">Description</label>
           <input v-model="course.description" type="text" id="description" />
         </div>
       </div>
+
       <br />
+
       <button class="success" name="editCourse" v-on:click.prevent="edit()">
         Edit
       </button>
