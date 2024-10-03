310cbde304382541a9bbaa969ac79ec2ece340a7
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2024-10-03 16:15:00 -0500

    Update CourseServices.js

--- src/services/CourseServices.js
+++ src/services/CourseServices.js
@@ -4,7 +4,7 @@ var baseurl = "";
 if (process.env.NODE_ENV === "development") {
   baseurl = "http://localhost:3016/course-t6";
 } else {
-  baseurl = "/course-t6";
+  baseurl = "localhost/course-t6";
 
 }
 
