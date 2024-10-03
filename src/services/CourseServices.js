758a3120e2524d9baec5d8f91005ef81fe3c8e9b
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2024-10-03 17:30:52 -0500

    Update CourseServices.js

--- src/services/CourseServices.js
+++ src/services/CourseServices.js
@@ -4,7 +4,7 @@ var baseurl = "";
 if (process.env.NODE_ENV === "development") {
   baseurl = "http://localhost:3016/course-t6";
 } else {
-  baseurl = "/course-t6";
+  baseurl = "/localhost/course-t6";
 
 }
 
@@ -17,22 +17,6 @@ const apiClient = axios.create({
     "Access-Control-Allow-Origin": "*",
     crossDomain: true,
   },
-  // transformRequest: (data, headers) => {
-  //   let token = localStorage.getItem("token");
-  //   let authHeader = "";
-  //   if (token != null && token != "") {
-  //     authHeader = "Bearer " + token;
-  //     headers["Authorization"] = authHeader;
-  //   }
-  //   return JSON.stringify(data);
-  // },
-  // transformResponse: function (data) {
-  //   data = JSON.parse(data);
-  //   if (!data.success && data.code == "expired-session") {
-  //     localStorage.removeItem("token");
-  //   }
-  //   return data;
-  // },
 });
 
 export default {
@@ -51,36 +35,4 @@ export default {
   deleteCourse(id) {
     return apiClient.delete("/courses/" + id);
   },
-  // getListItems(listId) {
-  //   return apiClient.get("lists/" + listId + "/items");
-  // },
-  // addListItem(listId, item) {
-  //   return apiClient.post("lists/" + listId + "/items", item);
-  // },
-  // updateListItem(listId, itemId, item) {
-  //   return apiClient.put("lists/" + listId + "/items/" + itemId, item);
-  // },
-  // deleteListItem(listId, itemId) {
-  //   return apiClient.delete("lists/" + listId + "/items/" + itemId);
-  // },
-  // getUser() {
-  //   return apiClient.get("users");
-  // },
-  // addUser(user) {
-  //   return apiClient.post("users", user);
-  // },
-  // loginUser(user) {
-  //   return apiClient.post("users/login", user, {
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //       "X-Requested-With": "XMLHttpRequest",
-  //       crossDomain: true,
-  //       Authorization: "Basic " + btoa(user.username + ":" + user.password),
-  //     },
-  //   });
-  // },
-  // logoutUser() {
-  //   return apiClient.post("users/logout");
-  // },
 };
